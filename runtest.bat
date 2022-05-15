695a1f5b05a8ed1adb34b0b7bdf9303597cb7cae
Ofey Chan <ofey206@gmail.com>
2022-05-15 21:21:34 +0800

    Basic plugin structure.

--- runtest.bat
+++ runtest.bat
@@ -1,4 +1,4 @@
 pyuic5.exe .\ui\mainwindow.ui -o .\ui\mainwindow.py
-pyuic5.exe .\ui\basicAnalysis.ui -o .\ui\basicAnalysis.py
+pyuic5.exe .\plugin\basicAnalysis\ui\basicAnalysis.ui -o .\plugin\basicAnalysis\ui\basicAnalysis.py
 
 python __main__.py
